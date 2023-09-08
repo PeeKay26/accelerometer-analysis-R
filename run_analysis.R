a78b8fddfe7f5c095cf02c7ed35ed221f4ad46fc
parthkotwal265@gmail.com <80135814+PeeKay26@users.noreply.github.com>
2023-09-07 19:03:05 -0700

    again

---
+++ run_analysis.R
@@ -0,0 +1,40 @@
+library(dplyr)
+filename <- "accelerometer_data.zip"
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",filename,method = "curl")
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+X <- rbind(x_train,x_test)
+Y <- rbind(y_train,y_test)
+Subject <- rbind(subject_train, subject_test)
+merged <- cbind(Subject, Y, X)
+tidy <- merged %>% select(subject,code,contains("mean"),contains("std"))
+tidy$code <- activities[tidy$code, 2]
+
+names(tidy)[2] = "activity"
+names(tidy)<-gsub("Acc", "Accelerometer", names(tidy))
+names(tidy)<-gsub("Gyro", "Gyroscope", names(tidy))
+names(tidy)<-gsub("BodyBody", "Body", names(tidy))
+names(tidy)<-gsub("Mag", "Magnitude", names(tidy))
+names(tidy)<-gsub("^t", "Time", names(tidy))
+names(tidy)<-gsub("^f", "Frequency", names(tidy))
+names(tidy)<-gsub("tBody", "TimeBody", names(tidy))
+names(tidy)<-gsub("-mean()", "Mean", names(tidy), ignore.case = TRUE)
+names(tidy)<-gsub("-std()", "STD", names(tidy), ignore.case = TRUE)
+names(tidy)<-gsub("-freq()", "Frequency", names(tidy), ignore.case = TRUE)
+names(tidy)<-gsub("angle", "Angle", names(tidy))
+names(tidy)<-gsub("gravity", "Gravity", names(tidy))
+
+FinalData <- TidyData %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(FinalData, "FinalData.txt", row.name=FALSE)
